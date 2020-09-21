337ffb4e093164cd9b640680964121da200d2451
Richard Zilahi <richard.zilahi@salesframe.com>
2020-09-21 23:04:25 +0300

    renamed

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import styled from "styled-components";
 import './App.css';
-import { motion, useMotionValue } from 'framer-motion'
-import move from "array-move";
+import arrayMove from 'array-move';
+import {SortableContainer, SortableElement} from '@alexcheuk/react-sortable-hoc';
 
 import { colors } from './coloors';
-import { findIndex, Position } from './findIndex';
 
 interface BoxxProps {
   backgroundColor: string;
 }
 
-const Boxx = styled(motion.div)<BoxxProps>`
+const Boxx = styled.div<BoxxProps>`
   padding: 50px;
   display: flex;
   justify-content: "center";
@@ -28,86 +27,50 @@ const Boxx = styled(motion.div)<BoxxProps>`
 interface BoxProps {
   backgroundColor: string;
   index: number;
-  setPosition: (i: number, offset: Position) => Position;
-  moveItem: any;
 }
 
 const Box = ({
   backgroundColor,
   index,
-  moveItem,
-  setPosition,
 }: BoxProps) => {
-  const [isDragging, setDragging] = useState(false);
-  const onTop = { zIndex: 1 };
-  const flat = {
-    zIndex: 0,
-    transition: { delay: 0.3 }
-  };
 
   const ref = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (ref.current !== null) {
-      setPosition(index, {
-        width: ref.current.clientWidth,
-				height: ref.current.clientHeight,
-				top: ref.current.offsetHeight,
-				bottom: ref.current.offsetTop,
-      });
-    }
-  });
-
-  const dragOrigin = useMotionValue(0);
-
   return (
     <Boxx
       ref={ref}
-      animate={isDragging ? onTop : flat}
       backgroundColor={backgroundColor}
-      whileHover={{ scale: 1.05 }}
-      dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
-      drag
-      dragElastic={1}
-      initial={false}
-      onDragStart={() => setDragging(true)}
-      onDragEnd={() => setDragging(false)}
-      onDrag={(e, { point }) => moveItem(index, point.y, point.x)}
-      layout="position"
-      transition={({ delta }: any) => {
-        dragOrigin.set(dragOrigin.get() + delta.x)
-						
-      }}
     >
       { index + 1}
     </Boxx>
   )
 }
 
-function App() {
-  const [boxes, setBoxes] = useState(colors)
-
-  const positions = useRef<Position[]>([]).current;
-  const setPosition = (i: number, offset: Position) => (positions[i] = offset);
-
-  const moveItem = (i: number, dragOffsetY: number, dragOffsetX: number) => {
-    const targetIndex = findIndex(i, dragOffsetY, dragOffsetX, positions);
-    if (targetIndex !== i) setBoxes(move(colors, i, targetIndex));
-  };
-
+const SortableItem = SortableElement(({backgroundColor, index}: BoxProps) => {
   return (
-    <div className="App">
+    <Box backgroundColor={backgroundColor} index={index} />
+  )
+});
+
+const SortableList = SortableContainer(({ items }: any) => {
+  return (
+    <ul>
       {
-        boxes.map((currColor, index) => (
-          <Box
-            key={currColor}
-            backgroundColor={currColor}
-            index={index}
-            setPosition={setPosition}
-            moveItem={moveItem}
-          />
+        items.map((value: string, index: number) => (
+          <SortableItem backgroundColor={value} index={index} />
         ))
       }
+    </ul>
+  );
+});
+
+function App() {
+  const [boxes, setBoxes] = useState(colors)
+  function onSortEnd ({oldIndex, newIndex}: any) {
+    setBoxes(arrayMove(boxes, oldIndex, newIndex))
+  };
+  return (
+    <div className="App">
+      <SortableList axis="xy" items={boxes} onSortEnd={({ oldIndex, newIndex}: any) => onSortEnd({oldIndex, newIndex})} />
     </div>
   );
 }
